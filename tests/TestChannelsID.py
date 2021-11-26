6919beb8f70247c8f4e4f02171d5fe39121c7551
alinashev <al.t.shevchenko@gmail.com>
2021-11-27 00:30:10 +0200

    Class ChannelsID covered

---
+++ tests/TestChannelsID.py
@@ -0,0 +1,19 @@
+import enum
+import unittest
+
+from ChannelsID import ChannelsID
+
+
+class TestChannelsID(unittest.TestCase):
+
+    def test_get_channels_id(self):
+        channel_id: enum.Enum = ChannelsID('channels.txt').get_channels_id()
+        values: list = [v.value for v in channel_id]
+        name: list = [n.name for n in channel_id]
+
+        self.assertEqual(type(channel_id), enum.EnumMeta)
+        self.assertIsNot(len(values), 0)
+        self.assertIsNot(len(name), 0)
+
+    if __name__ == '__main__':
+        unittest.main()
