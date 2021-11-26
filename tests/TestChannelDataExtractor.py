edd8917fa5fc3871dc7448ed70796d60d5b5ef75
alinashev <al.t.shevchenko@gmail.com>
2021-11-27 00:32:14 +0200

    Class ChannelDataExtractor covered

---
+++ tests/TestChannelDataExtractor.py
@@ -0,0 +1,23 @@
+import unittest
+from enum import Enum
+
+from ChannelsID import ChannelsID
+from Extract.ChannelDataExtractor import ChannelDataExtractor
+
+
+class TestChannelDataExtractor(unittest.TestCase):
+    channel_id: Enum
+    extract_data: dict
+
+    def setup(self):
+        self.channel_id = ChannelsID("channels.txt").get_channels_id()
+        self.extract_data = ChannelDataExtractor().extract(self.channel_id)
+
+    def test_extract(self):
+        self.setup()
+
+        self.assertIsNot(len(self.extract_data), 0)
+        self.assertEqual(type(self.extract_data), dict)
+
+    if __name__ == '__main__':
+        unittest.main()
