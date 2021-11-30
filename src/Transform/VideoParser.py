3d006a8cdd085d72ddf51643f0aaea8910cfb19c
alinashev <al.t.shevchenko@gmail.com>
2021-11-30 11:37:24 +0200

    Added handling of missing value for videoId

--- src/Transform/VideoParser.py
+++ src/Transform/VideoParser.py
@@ -6,12 +6,20 @@ from Transform.Parser import Parser
 
 class VideoParser(Parser):
     def parse_to_obj(self, json_string: Any, ChannelsID: Any) -> list[Video]:
-        return [Video(channelID.name,
-                      channelID.value,
-                      str(None),
-                      json_string[channelID.name]['items'][i]['snippet']['publishedAt'],
-                      json_string[channelID.name]['items'][i]['snippet']['title'],
-                      str(None)
-                      )
-                for channelID in ChannelsID
-                for i in range(0, 5)]
+        obj_list = list()
+        for channelID in ChannelsID:
+            for i in range(0, 5):
+                try:
+                    res: str = (json_string[channelID.name]['items'][i]['id']['videoId'])
+                except:
+                    res = str(None)
+
+                obj_list.append(Video(channelID.name,
+                                      channelID.value,
+                                      res,
+                                      json_string[channelID.name]['items'][i]['snippet']['publishedAt'],
+                                      json_string[channelID.name]['items'][i]['snippet']['title'],
+                                      str(None)
+                                      )
+                                )
+        return obj_list
