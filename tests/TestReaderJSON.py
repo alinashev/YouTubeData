d9f857f9fa8589ecedca3e2497f3f108c3b62243
alinashev <al.t.shevchenko@gmail.com>
2021-11-30 22:09:25 +0200

    Fixed TestStorageS3 tests and TestReaderJSON

--- tests/TestReaderJSON.py
+++ tests/TestReaderJSON.py
@@ -1,47 +1,48 @@
 import unittest
-from typing import Any
 
 from Commons.ReaderJSON import ReaderJSON
-from Commons.StorageS3 import StorageS3
 
 
 class TestReaderJSON(unittest.TestCase):
-    directory: str = "YouTube"
-    storage: StorageS3
     path: list
     json: dict
+
+    reader_channels: ReaderJSON
+    reader_video: ReaderJSON
+
+    open_channels: str
+    open_video: str
+
     json_channels: dict
     json_video: dict
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.storage = StorageS3()
-        cls.storage.download_folder(cls.directory)
-        cls.path = cls.storage.get_path_list()
+    def setUp(self) -> None:
+        self.reader_channels = ReaderJSON('YouTube/Lake/jsonTypesFile/YouTube/dataVideos.json')
+        self.reader_video = ReaderJSON('YouTube/Lake/jsonTypesFile/YouTube/dataVideos.json')
 
-        cls.json_channels = ReaderJSON(cls.path[0]).get_json()
-        cls.json_video = ReaderJSON(cls.path[1]).get_json()
+        self.open_channels = self.reader_channels.open_json_file()
+        self.open_video = self.reader_video.open_json_file()
 
-        cls.result_channels: Any = ReaderJSON(cls.path[0]).open_json_file()
-        cls.result_video: Any = ReaderJSON(cls.path[1]).open_json_file()
+        self.json_channels = self.reader_channels.get_json()
+        self.json_video = self.reader_video.get_json()
 
     def test_return_type_open_dataChannels(self) -> None:
-        self.assertEqual(type(self.result_channels), dict)
+        self.assertEqual(type(self.open_channels), dict)
 
     def test_return_type_open_dataVideos(self) -> None:
-        self.assertEqual(type(self.result_video), dict)
+        self.assertEqual(type(self.open_video), dict)
 
     def test_empty_result_dataChannels(self) -> None:
-        self.assertIsNot(len(self.result_channels), 0)
+        self.assertIsNot(len(self.open_channels), 0)
 
     def test_empty_result_dataVideos(self) -> None:
-        self.assertIsNot(len(self.result_video), 0)
+        self.assertIsNot(len(self.open_video), 0)
 
-    def test_keys_amount_dataChannels(self) -> None:
-        self.assertIs(len(self.result_channels.keys()), 10)
+    def test_keys_amount_json_channels_file(self) -> None:
+        self.assertIs(len(self.json_channels.keys()), 10)
 
-    def test_keys_amount_dataVideos(self) -> None:
-        self.assertIs(len(self.result_video.keys()), 10)
+    def test_keys_amount_json_video_file(self) -> None:
+        self.assertIs(len(self.json_video.keys()), 10)
 
     def test_type_returned_json_channels_file(self) -> None:
         self.assertEqual(type(self.json_channels), dict)
--- tests/TestStorageS3.py
+++ tests/TestStorageS3.py
@@ -1,23 +1,30 @@
 import unittest
+from typing import Any
+from unittest.mock import patch
 
 from Commons.StorageS3 import StorageS3
 
 
 class TestStorageS3(unittest.TestCase):
     directory: str = "YouTube"
-    storage: StorageS3
+    storage: Any
 
     def setUp(self) -> None:
         self.storage = StorageS3()
         self.storage.download_folder(self.directory)
+        self.result_path_list: list = self.storage.get_path_list()
 
-    def test_get_path_list(self) -> None:
-        result_path_list: list = self.storage.get_path_list(self.directory)
+    def test_existence_dataChannels(self) -> None:
+        self.assertIsNot(self.result_path_list[0].find('dataChannels'), -1)
 
-        self.assertIsNot(result_path_list[0].find('dataChannels'), -1)
-        self.assertIsNot(result_path_list[1].find('dataVideos'), -1)
-        self.assertIsNot(len(result_path_list), 0)
-        self.assertEqual(type(result_path_list), list)
+    def test_existence_dataVideos(self) -> None:
+        self.assertIsNot(self.result_path_list[1].find('dataVideos'), -1)
+
+    def test_existence_files(self) -> None:
+        self.assertIsNot(len(self.result_path_list), 0)
+
+    def test_type_returned(self) -> None:
+        self.assertEqual(type(self.result_path_list), list)
 
     if __name__ == '__main__':
         unittest.main()
