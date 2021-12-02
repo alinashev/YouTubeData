804180530f83045dd8000bb027c7e680faffdce8
alinashev <al.t.shevchenko@gmail.com>
2021-12-02 10:47:44 +0200

    Added throwing exceptions for ReaderJSON

--- src/Commons/ReaderJSON.py
+++ src/Commons/ReaderJSON.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 
 class ReaderJSON:
@@ -6,11 +7,16 @@ class ReaderJSON:
     text: dict
 
     def __init__(self, path: str) -> None:
+        if not os.path.exists(path):
+            raise FileNotFoundError('File not found')
         self.path = path
 
     def open(self) -> dict:
         with open(self.path, 'r', encoding='utf-8') as f:
-            self.text = json.load(f)
+            try:
+                self.text = json.load(f)
+            except ValueError as e:
+                raise ValueError('Invalid json: {}'.format(e)) from None
         return self.text
 
     def get_json(self) -> dict:
