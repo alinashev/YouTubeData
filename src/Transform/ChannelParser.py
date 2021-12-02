30a2e1d1badcfbd5d3d9ee044c25b907634f3404
alinashev <al.t.shevchenko@gmail.com>
2021-12-02 11:49:59 +0200

    Fixed ChannelParser. Added tests

--- src/Transform/ChannelParser.py
+++ src/Transform/ChannelParser.py
@@ -7,7 +7,7 @@ from Transform.Parser import Parser
 class ChannelParser(Parser):
     def parse(self, json_string: Any, ChannelsID: dict) -> list[Channel]:
         return [Channel(channelID,
-                        ChannelsID[channelID].value,
+                        ChannelsID[channelID],
                         json_string[channelID]["items"][0]["statistics"]["viewCount"],
                         json_string[channelID]["items"][0]["statistics"]["subscriberCount"],
                         json_string[channelID]["items"][0]["statistics"]["videoCount"]
