f1108b372581a6fea669cfd4a1672839166e89d4
alinashev <al.t.shevchenko@gmail.com>
2021-12-05 01:35:50 +0200

    Changed TestVideoDataExtractor

--- tests/TestVideoDataExtractor.py
+++ tests/TestVideoDataExtractor.py
@@ -4,12 +4,9 @@ from Extract.VideoDataExtractor import VideoDataExtractor
 
 
 class TestVideoDataExtractor(unittest.TestCase):
-    channel_id: dict
-    extract_data: dict
 
     def setUp(self) -> None:
         self.channel_id = {line.split()[0]: line.split()[1] for line in open('resources/channels.txt')}
-
         self.extract_data = VideoDataExtractor().extract(self.channel_id)
 
     def test_empty(self) -> None:
