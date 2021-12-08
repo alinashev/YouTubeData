c75d26fb82ac06905b6d3478680097796da98400
alinashev <al.t.shevchenko@gmail.com>
2021-12-08 21:42:38 +0200

    Added creating database tables

---
+++ src/SQLCode/videocategory .sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS public."videocategory"
+(
+    id serial NOT NULL,
+    video_id character varying,
+    video_category_id character varying,
+    channel_name character varying,
+    channel_id character varying,
+    PRIMARY KEY (id)
+);
